0b138579c9c23654d304ada555d883b926e0a88a
vakor <myksv200@gmail.com>
2024-05-16 16:37:16 +0300

    constants: colors + icons

---
+++ constants/Colors.jsx
@@ -0,0 +1,32 @@
+export const Colors = {
+    main: '#080A1A',
+    subMain: '#ff0000',
+    dry: '#0b1133',
+    star: "#FFB000",
+    text: "#c0c0c0",
+    border: '#243970',
+    dryGray: '#E0D5D5',
+    noPosterAvailable: '#E2E2E2',
+    white: '#ffffff',
+}
+
+// export const Colors = {
+//   light: {
+//     text: '#11181C',
+//     background: '#fff',
+//     tint: tintColorLight,
+//     icon: '#687076',
+//     tabIconDefault: '#687076',
+//     tabIconSelected: tintColorLight,
+//   },
+//   dark: {
+//     text: '#ECEDEE',
+//     background: '#151718',
+//     tint: tintColorDark,
+//     icon: '#9BA1A6',
+//     tabIconDefault: '#9BA1A6',
+//     tabIconSelected: tintColorDark,
+//   },
+// };
+
+
---
+++ constants/icons.js
@@ -0,0 +1,15 @@
+import logoIco from '../assets/icons/favicon.ico';
+import logoPng from '../assets/icons/favicon.png';
+import logo16 from '../assets/icons/favicon-16x16.png';
+import logo32 from '../assets/icons/favicon-32x32.png';
+import logo192 from '../assets/icons/favicon-192x192.png';
+import logo512 from '../assets/icons/favicon-512x512.png';
+
+export default {
+    logoIco,
+    logoPng,
+    logo16,
+    logo32,
+    logo192,
+    logo512
+}
