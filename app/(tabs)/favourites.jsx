20212468db5ca3ccce3dc63305da3e905a16d2bc
milka_deluxe <milagalko1@gmail.com>
2024-05-19 23:37:48 +0300

    :sparkles: Favourites tab

--- app/(tabs)/favourites.jsx
+++ app/(tabs)/favourites.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {ScrollView, View} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
+import Title from "../../components/Title";
+import {FontAwesome} from "@expo/vector-icons";
+import {Colors} from "../../constants/Colors";
+import MoviesBlock from "../../components/media/MoviesBlock";
+import {useAuth} from "../../context/AuthProvider";
 
 const Favourites = () => {
+    const {user} = useAuth()
+    const icon = () => <FontAwesome name="heart" size={24} color={Colors.subMain}/>
+    // TODO: Implement GetMoviesByRequest function
+    const favoriteMovies = user?.favoriteMovies
+
     return (
-        <SafeAreaView className={'flex-1'}>
-            <Text>
-                Favourites
-            </Text>
+        <SafeAreaView className={'relative flex-1 bg-main'}>
+            <ScrollView contentContainerStyle={{flexGrow: 1}}>
+                <View className="items-center m-3">
+                    <MoviesBlock title='Your Favourites' icon={icon}/>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 };
--- components/media/MoviesBlock.jsx
+++ components/media/MoviesBlock.jsx
@@ -9,33 +9,6 @@ const MoviesBlock = ({title, request, movieCount, icon}) => {
     // const {GetMoviesByRequest} = MovieContextConsumer()
     // const Movies = GetMoviesByRequest(request, movieCount);
 
-    const items = [
-        {
-            id: 1,
-            title: 'Item 1',
-            backgroundColor: 'red',
-        },
-        {
-            id: 2,
-            title: 'Item 2',
-            backgroundColor: 'green',
-        },
-        {
-            id: 3,
-            title: 'Item 3',
-            backgroundColor: 'blue',
-        },
-        {
-            id: 4,
-            title: 'Item 4',
-            backgroundColor: 'yellow',
-        },
-        {
-            id: 5,
-            title: 'Item 5',
-            backgroundColor: 'orange',
-        },
-    ];
     const Movies = tmdbMovies20;
     return (
         <View className='w-full'>
