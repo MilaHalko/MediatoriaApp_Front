994277e0ab9fe27ff35e7cb5c9078fb7c1104934
milka_deluxe <milagalko1@gmail.com>
2024-06-04 13:47:47 +0300

    :fire: Remove validation

--- api/validation.js
+++
@@ -1,9 +0,0 @@
-export const urlIsValid = async (URL) => {
-    try {
-        const res = await fetch(URL)
-        return res.status >= 200 && res.status < 300
-    } catch (e) {
-        console.log(e)
-        return false;
-    }
-}
