cb0d913ce138617a025a38f08dc76d609cc58174
milka_deluxe <milagalko1@gmail.com>
2024-06-08 09:58:13 +0300

    :hammer: Add users to layout

--- app/(sidebar)/_layout.jsx
+++ app/(sidebar)/_layout.jsx
@@ -1,15 +1,17 @@
 import {Stack} from "expo-router";
 import {StatusBar} from "expo-status-bar";
+import {Colors} from "../../constants/Colors";
 
 const SidebarLayout = () => {
     return (
         <>
             <Stack screenOptions={{headerShown: false}}>
                 <Stack.Screen name="upcoming"/>
+                <Stack.Screen name="users"/>
             </Stack>
             <StatusBar backgroundColor={Colors.main} style="light"/>
         </>
     );
 };
 
-export default MovieLayout;
+export default SidebarLayout;
